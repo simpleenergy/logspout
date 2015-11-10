0de5d054cb43fe6fc7f254da503371940c3c4c3e
Levi DeHaan <levi@simpleenergy.com>
2015-11-10 03:36:54 -0700

    adding logstash support

--- modules.go
+++ modules.go
@@ -8,4 +8,6 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
 	_ "github.com/gliderlabs/logspout/transports/tls"
+	_ "github.com/looplab/logspout-logstash"
+	_ "github.com/gliderlabs/logspout/transports/udp"
 )
